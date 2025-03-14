72d06eb711d8061c173e13cd244d97e3e7b39591
Olexandr <akarakai20@gmail.com>
2025-03-14 11:03:31 +0200

    wip

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,20 +1,20 @@
-//const baseApiURL = 'https://st.wooh.cc'
-const baseApiURL = 'http://vaskania-new.loc'
+const baseApiURL = 'https://vs-admin-wp.system-25.com'
+//const baseApiURL = 'http://vaskania-new.loc'
 const restApiURL = '/wp-json/vs-calculator/v1'
 const apiDistDir = baseApiURL + '/wp-content/plugins/vaskaniya-calc/dist'
 
 export const configConstants = {
-	baseApiURL,
-	restApiURL,
-	apiDistDir,
-	apiAssetsDir: apiDistDir + '/assets',
-	apiImagesDir: apiDistDir + '/assets/images',
+    baseApiURL,
+    restApiURL,
+    apiDistDir,
+    apiAssetsDir: apiDistDir + '/assets',
+    apiImagesDir: apiDistDir + '/assets/images',
 }
 
 export class Config {
-	static getApiImagesDir(url?: string) {
-		if (url) return configConstants.apiImagesDir + url
+    static getApiImagesDir(url?: string) {
+        if (url) return configConstants.apiImagesDir + url
 
-		return configConstants.baseApiURL + configConstants.apiImagesDir
-	}
+        return configConstants.baseApiURL + configConstants.apiImagesDir
+    }
 }
